4c6c1bf4b43d4047de00a8e4e5bf24f4e59844fe
wolframkriesing <wolfram+github@kriesing.de>
2017-12-12 18:22:24 +0100

    Also remove it in the currently unused JS.

--- js/new-age.js
+++ js/new-age.js
@@ -9,7 +9,7 @@
       if (target.length) {
         $('html, body').animate({
           scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
+        }, 1000);
         return false;
       }
     }
