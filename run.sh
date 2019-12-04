7a4367c72585559c4fb7f288fb86e2dce0b51b95
Preetham N K <preetham.thejeswi@holidaycheck.com>
2019-12-04 19:33:49 +0100

    Renamed WORKON to WORKDIR, made command multi-line

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ CONTENT_DIR="_apprenticeship"
 IMAGEID="apprenticeship_web"
 HOST_PORT=4000
 DOCKER_PORT=4000
+WORKDIR=/srv/ # docker WORKDIR
 
 docker build -t $IMAGEID .
-docker run -p $HOST_PORT:$DOCKER_PORT -v $(pwd)/$CONTENT_DIR:/srv/$CONTENT_DIR $IMAGEID
+docker run -p $HOST_PORT:$DOCKER_PORT \
+       -v $(pwd)/$CONTENT_DIR:$WORKDIR$CONTENT_DIR \
+       $IMAGEID
