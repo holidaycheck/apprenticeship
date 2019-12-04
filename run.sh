8688baed69efc3c2383291052cf19715204bb066
Preetham N K <preetham.thejeswi@holidaycheck.com>
2019-12-04 19:48:09 +0100

    Added modes to build and run seperately
    `./run.sh build` builds the docker image.
    `./run.sh run` run the built image.
    
    The logic behind this is that you'd hardly need to
    rebuild your image, as it is not so common to add new
    Gems. While it is common to run Jekyll quite often.
    Changes to the `md` files will be reflected without
    re-building or even re-running.

--- run.sh
+++ run.sh
@@ -4,9 +4,19 @@ CONTENT_DIR="_apprenticeship"
 IMAGEID="apprenticeship_web"
 HOST_PORT=4000
 DOCKER_PORT=4000
-WORKDIR=/srv/ # docker WORKDIR
+WORKDIR=/srv/
 
-docker build -t $IMAGEID .
-docker run -p $HOST_PORT:$DOCKER_PORT \
-       -v $(pwd)/$CONTENT_DIR:$WORKDIR$CONTENT_DIR \
-       $IMAGEID
+case $1 in
+    "build")
+	docker build -t $IMAGEID .
+	;;
+    "run")
+	docker run -p $HOST_PORT:$DOCKER_PORT \
+	       -v $(pwd)/$CONTENT_DIR:$WORKDIR$CONTENT_DIR \
+	       $IMAGEID
+	;;
+    *)
+	echo "Invalid usage!"
+	echo "To build the docker image, use: ./run.sh build"
+	echo "To run the docker image, use: ./run.sh run"
+esac
