8aafc89ca9013b2806aa24e73a0ed3d586902e43
Preetham N K <preetham.thejeswi@holidaycheck.com>
2019-12-04 19:23:41 +0100

    Refactored shell script to meaning of values more explicit

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-docker build -t apprenticeship_web .
-docker run -p 4000:4000 -v "$(pwd)"/_apprenticeship:/srv/_apprenticeship app_web
+#!/bin/sh
+
+CONTENT_DIR="_apprenticeship"
+IMAGEID="apprenticeship_web"
+HOST_PORT=4000
+DOCKER_PORT=4000
+
+docker build -t $IMAGEID .
+docker run -p $HOST_PORT:$DOCKER_PORT -v $(pwd)/$CONTENT_DIR:/srv/$CONTENT_DIR $IMAGEID
